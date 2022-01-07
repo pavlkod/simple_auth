54feb2d3ba58d2cb38ef5b194cba12708cff48b0
Pavel.K <pavelweb74@gmail.com>
2022-01-07 16:14:09 +0500

    Add express in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT, () => console.log("Server started"));
