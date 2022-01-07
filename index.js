bc7c089695f40e96a185d9912510cb6d10d424cc
Pavel.K <pavelweb74@gmail.com>
2022-01-07 17:16:40 +0500

    Add mongoose

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
 
-app.listen(process.env.PORT, () => console.log("Server started"));
+(async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(process.env.PORT, () => console.log("Server started"));
+  } catch (e) {
+    console.log(e);
+  }
+})();
