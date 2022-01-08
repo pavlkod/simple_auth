535c09ed16641e8d825e10c5b2fa415e74df4c77
Pavel.K <pavelweb74@gmail.com>
2022-01-08 17:28:43 +0500

    Implement registration in AuthController

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -2,5 +2,12 @@ class AuthController {
   auth(req, res) {
     res.send({ message: "OK" });
   }
+  async registration(req, res) {
+    try {
+      res.send("REG");
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
 module.exports = new AuthController();
