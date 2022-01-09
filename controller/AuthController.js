017122a159952e89ea66c81265a9ae140a694fec
Pavel.K <pavelweb74@gmail.com>
2022-01-09 16:45:35 +0500

    Refactor registration on User model

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -7,7 +7,13 @@ class AuthController {
   }
   async registration(req, res) {
     try {
-      res.send("REG");
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+      const user = await User.create({ username, password });
+      res.send(user);
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: "Registration error" });
