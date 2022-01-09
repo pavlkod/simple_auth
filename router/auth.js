b7d801676b0429d4f0482bd5f7bc69fc329cc91e
Pavel.K <pavelweb74@gmail.com>
2022-01-09 20:43:15 +0500

    Add registration router

--- router/auth.js
+++ router/auth.js
@@ -3,5 +3,6 @@ const Router = require("express");
 const router = new Router();
 
 router.get("/", AuthController.auth);
+router.post("/registration", AuthController.registration);
 
 module.exports = router;
