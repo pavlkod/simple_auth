03cd6e1bf6fdfe354e1ad8c1894735e5880cd44f
Pavel.K <pavelweb74@gmail.com>
2022-01-13 21:18:29 +0500

    Add middleware for registration router

--- router/auth.js
+++ router/auth.js
@@ -2,7 +2,16 @@ const AuthController = require("../controller/AuthController");
 const Router = require("express");
 const router = new Router();
 
+const { check } = require("express-validator");
+
 router.get("/", AuthController.auth);
-router.post("/registration", AuthController.registration);
+router.post(
+  "/registration",
+  [
+    check("username", "Username is required"),
+    check("password", "Minimum 4 symbols max 10").isLength({ min: 4, max: 10 }),
+  ],
+  AuthController.registration
+);
 
 module.exports = router;
