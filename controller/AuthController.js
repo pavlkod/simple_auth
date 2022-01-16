0eb864d146472c2cb89bd03260b6c77207bb4c9a
Pavel.K <pavelweb74@gmail.com>
2022-01-16 12:28:01 +0500

    Add errors

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -12,7 +12,7 @@ class AuthController {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+        return res.status(400).json({ message: errors });
       }
 
       const { username, password } = req.body;
